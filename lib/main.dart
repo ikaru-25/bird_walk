05d3cbba5595cb9d51ffea4584e3d05bedf2bf96
Ito Kaoru <75163339+ikaru-25@users.noreply.github.com>
2023-01-22 18:05:03 +0900

    ログイン画面追加
    ホーム画面だったエリア一覧はログイン後の画面に移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // main.dart
-import 'package:flutter/material.dart';
-import 'model/field.dart';
 
-void main() {
+import 'package:bird_walk/area_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -18,182 +24,90 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.teal,
         ),
-        home: const HomePage());
+        home: AuthPage());
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class AuthPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _AuthPageState createState() => _AuthPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  // All journals
-  List<Map<String, dynamic>> _fields = [];
-
-  bool _isLoading = true;
-  // This function is used to fetch all data from the database
-  void _refreshFieldList() async {
-    final data = await FieldTBL.getFields();
-    setState(() {
-      _fields = data;
-      _isLoading = false;
-    });
-  }
-
-  // wigetが作成されたタイミングの処理
-  @override
-  void initState() {
-    super.initState();
-    _refreshFieldList(); // Loading the diary when the app starts
-  }
-
-  final TextEditingController _fieldNameController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-
-  // This function will be triggered when the floating button is pressed
-  // It will also be triggered when you want to update an item
-  void _showForm(int? id) async {
-    if (id != null) {
-      // id == null -> create new item
-      // id != null -> update an existing item
-      final existingJournal =
-          _fields.firstWhere((element) => element['id'] == id);
-      _fieldNameController.text = existingJournal['name'];
-      _descriptionController.text = existingJournal['description'];
-    }
-
-    showModalBottomSheet(
-        context: context,
-        elevation: 5,
-        isScrollControlled: true,
-        builder: (_) => Container(
-              padding: EdgeInsets.only(
-                top: 15,
-                left: 15,
-                right: 15,
-                // this will prevent the soft keyboard from covering the text fields
-                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    controller: _fieldNameController,
-                    decoration: const InputDecoration(hintText: 'Name'),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: _addressController,
-                    decoration: const InputDecoration(hintText: 'Address'),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    controller: _descriptionController,
-                    decoration: const InputDecoration(hintText: 'Description'),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      // Save new journal
-                      if (id == null) {
-                        await _addField();
-                      }
-
-                      if (id != null) {
-                        await _updateField(id);
-                      }
-
-                      // Clear the text fields
-                      _fieldNameController.text = '';
-                      _addressController.text = '';
-                      _descriptionController.text = '';
-
-                      // Close the bottom sheet
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(id == null ? 'Create New' : 'Update'),
-                  )
-                ],
-              ),
-            ));
-  }
-
-// Insert
-  Future<void> _addField() async {
-    await FieldTBL.createField(
-        _fieldNameController.text, _addressController.text,  _descriptionController.text);
-    _refreshFieldList();
-  }
-
-  // Update
-  Future<void> _updateField(int id) async {
-    await FieldTBL.updateField(
-        id, _fieldNameController.text, _addressController.text,  _descriptionController.text);
-    _refreshFieldList();
-  }
-
-  // Delete
-  void _deleteField(int id) async {
-    await FieldTBL.deleteField(id);
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Successfully deleted a journal!'),
-    ));
-    _refreshFieldList();
-  }
+class _AuthPageState extends State<AuthPage> {
+  // 入力されたメールアドレス
+  String newUserEmail = "";
+  // 入力されたパスワード
+  String newUserPassword = "";
+  // 登録・ログインに関する情報を表示
+  String infoText = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BirdWalk'),
+        title: const Text('BirdWalk__'),
       ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: _fields.length,
-              itemBuilder: (context, index) => Card(
-                color: Colors.teal[200],
-                margin: const EdgeInsets.all(15),
-                child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage("assets/aioiyama.JPG"),
-                    ),
-                    title: Text(_fields[index]['name']),
-                    subtitle: Text(_fields[index]['description']),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () => _showForm(_fields[index]['id']),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () =>
-                                _deleteField(_fields[index]['id']),
-                          ),
-                        ],
-                      ),
-                    )),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                // テキスト入力のラベルを設定
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
               ),
-            ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => _showForm(null),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                // パスワードが見えないようにする
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AreaListPage()),
+                  );
+
+                  try {
+                    // メール/パスワードでユーザー登録
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                        await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail,
+                      password: newUserPassword,
+                    );
+
+                    // 登録したユーザー情報
+                    final User user = result.user!;
+                    setState(() {
+                      infoText = "登録OK：${user.email}";
+                    });
+                  } catch (e) {
+                    // 登録に失敗した場合
+                    setState(() {
+                      infoText = "登録NG：${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("ユーザー登録"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText)
+            ],
+          ),
+        ),
       ),
     );
   }
